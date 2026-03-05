d3f684353c8e6e14db0ac3a217337c8a68f65efa
SilentCrouwd <chris.buchholz.lauenau@gooogle.com>
2026-03-05 14:26:33 +0100

    Update repositoryInfo descriptions and refine animation durations for improved user experience

--- script.js
+++ script.js
@@ -176,7 +176,9 @@ function loadgit() {
 const repositoryInfo = [
   {
     id: 0,
-    info: "Von der Datenbank bis zum letzten Pixel alles im Griff.",
+    info: "Das Projekt ist eine interaktive Web-Schnittstelle für Authentifizierung, die einen physischen Kartenstapel imitiert. Anstatt klassische Seitenwechsel zu nutzen, werden die verschiedenen Ansichten (Willkommen, Login, Registrierung) durch CSS-Animationen ein- und ausgeschoben.",
+    infoCss:
+      "Optik: Das Design nutzt einen modernen Look mit radialen Verläufen und starken Schatten (box-shadow), um Tiefe zu erzeugen. Orange (#ff6d19) dient als konsistente Akzentfarbe.Animationen: * @keyframes switch: Simuliert das Aussortieren einer Karte, indem sie zur Seite gleitet und sich dann mit verändertem z-index wieder in den Stapel einfügt.",
   },
   { id: 1, info: "Code schreiben, den nicht nur der Compiler versteht." },
   {
@@ -190,7 +192,8 @@ const repositoryInfo = [
   },
   { id: 5, info: "Layouts zähmen, ohne den Überblick zu verlieren." },
 ];
-
+// ########################## DIscription #######################
+// Discription
 // Selektiert den Container (das Eltern-Element), in dem die Beschreibung erscheinen soll
 const infounsichtbar = document.getElementById("unsichtbar");
 
@@ -212,13 +215,51 @@ discriptionIcon.forEach((element, index) => {
       repoInfo.className = "p repoinfo";
       repoInfo.classList.add("animationGalerie");
       infounsichtbar.appendChild(repoInfo);
+      img[index].classList.remove(
+        "animationGalierereverse",
+        "animationGalerie",
+      );
+      void img[index].offsetWidth; // hier muss der browser eine pause haben sonst erkennt er nicht das die klasse eine andere ist
 
       img[index].classList.add("animationGalierereverse");
-
     } else {
       infounsichtbar.innerHTML = "";
-img[index].classList.remove("animationGalierereverse", "animationGalerie");
-      void img[index].offsetWidth;// hier muss der browser eine pause haben sonst erkennt er nicht das die klasse eine andere ist 
+      img[index].classList.remove(
+        "animationGalierereverse",
+        "animationGalerie",
+      );
+      void img[index].offsetWidth; // hier muss der browser eine pause haben sonst erkennt er nicht das die klasse eine andere ist
+      img[index].classList.add("animationGalerie");
+    }
+  });
+});
+//  CSS Discription
+
+const cssImg = document.querySelectorAll(".css-galerie");
+
+cssImg.forEach((element, index) => {
+  element.addEventListener("click", () => {
+    if (infounsichtbar.textContent === "") {
+      const cssInfo = document.createElement("p");
+
+      cssInfo.textContent = repositoryInfo[index].infoCss;
+      cssInfo.className = "p repoinfo";
+      cssInfo.classList.add("animationGalerie");
+      infounsichtbar.appendChild(cssInfo);
+      img[index].classList.remove(
+        "animationGalierereverse",
+        "animationGalerie",
+      );
+      void img[index].offsetWidth; // hier muss der browser eine pause haben sonst erkennt er nicht das die klasse eine andere ist
+
+      img[index].classList.add("animationGalierereverse");
+    } else {
+      infounsichtbar.innerHTML = "";
+      img[index].classList.remove(
+        "animationGalierereverse",
+        "animationGalerie",
+      );
+      void img[index].offsetWidth; // hier muss der browser eine pause haben sonst erkennt er nicht das die klasse eine andere ist
       img[index].classList.add("animationGalerie");
     }
   });
